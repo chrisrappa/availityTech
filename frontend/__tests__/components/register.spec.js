b853f167d09118293b7dee87c857731fe6ce5c20
chrisrappa <rappaproductionsinc@gmail.com>
2021-10-31 18:06:05 -0500

    added test conditions

---
+++ frontend/__tests__/components/register.spec.js
@@ -0,0 +1,21 @@
+import Register from '../components/Register';
+
+const register = Register;
+
+test('Screen should render properly when loaded', () => {
+
+
+  
+})
+
+test('When firstName field is filled out, state of firstName variable should change to entered text', () => {
+
+  
+
+})
+
+test('When submit button is clicked submitHandler should dispatch info in state variables', () => {
+
+
+
+})
---
+++ frontend/__tests__/reducers/userReducers.spec.js
@@ -0,0 +1,19 @@
+import userRegisterReducer from '../reducers/userReducer';
+
+const userReducer = userRegisterReducer;
+
+test('when action type is USER_REGISTER_REQUEST should return loading true', () => {
+
+
+})
+
+test('when action type is USER_REGISTER_SUCCESS should return loading false and userInfo', () => {
+  
+  
+})
+
+
+test('when action type is USER_REGISTER_FAIL should return loading false and error', () => {
+  
+
+})
